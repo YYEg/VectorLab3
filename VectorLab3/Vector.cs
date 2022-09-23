f31f0ee93bd0a3dc4e4a658898c5edd51fe85eda
YuEgor <rezervdesu15@gmail.com>
2022-09-23 17:19:48 +0800

    класс Vector

---
+++ VectorLab3/Vector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorLab3
+{
+    public enum CoordinateType {x, y, z};
+    internal class Vector
+    {
+        private double coordinate;
+        private CoordinateType type;
+
+        public Vector(double coordinate, CoordinateType type)
+        {
+            this.coordinate = coordinate;
+            this.type = type;
+        }
+
+        public string Verbose()
+        {
+            string typeVerbose = "";
+        }
+    }
+}
