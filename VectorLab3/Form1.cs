960b3236544d663eb5e18bb1345091f99dd56fcf
YuEgor <rezervdesu15@gmail.com>
2022-09-26 22:48:28 +0800

    все очишено

--- VectorLab3/Form1.cs
+++ VectorLab3/Form1.cs
@@ -16,4 +16,5 @@ namespace VectorLab3
         {
             InitializeComponent();
         }
+    }
 }
--- VectorLab3/Vector.cs
+++ VectorLab3/Vector.cs
@@ -18,66 +18,6 @@ namespace VectorLab3
         public double Verbose()
         {
             return this.coordinate;
-        }
-        //Возведение в степень
-        public static Vector Extent(Vector number, double powNumber)
-        {
-            var newNumber = Math.Pow(number.coordinate, powNumber);
-            var extentNumber = new Vector(newNumber);
-            return extentNumber;
-        }
-        //Корень квадратный
-        public static Vector Radical(Vector number)
-        {
-            var newNumber = Math.Sqrt(number.coordinate);
-            var extentNumber = new Vector(newNumber);
-            return extentNumber;
-        }
-        //Операция сложения векторов
-        public static Vector operator +(Vector instance, double number)
-        {
-            var newCoordinate = instance.coordinate + number;
-            var coordinateX2 = new Vector(newCoordinate);
-            return coordinateX2;
-        }
-        public static Vector operator +(double number, Vector instance)
-        {
-            return instance + number;
-        }
-        public static Vector operator +(Vector instance1, Vector instance2)
-        {
-            return instance1 + instance2.coordinate;
-        }
-        //Операция вычитания векторов
-        public static Vector operator -(Vector instance, double number)
-        {
-            var newCoordinate = instance.coordinate - number;
-            var coordinateX2 = new Vector(newCoordinate);
-            return coordinateX2;
-        }
-        public static Vector operator -(double number, Vector instance)
-        {
-            return instance - number;
-        }
-        public static Vector operator-(Vector instance1, Vector instance2)
-        {
-            return instance1 - instance2.coordinate;
-        }
-        //Операции умножения векторов
-        public static Vector operator *(Vector instance, double number)
-        {
-            var newCoordinate = instance.coordinate * number;
-            var coordinateX2 = new Vector(newCoordinate);
-            return coordinateX2;
-        }
-        public static Vector operator *(double number, Vector instance)
-        {
-            return instance * number;
-        }
-        public static Vector operator *(Vector instance1, Vector instance2)
-        {
-            return instance1 * instance2.coordinate;
-        }
-        
+        }                
     }
 }
