30204dcc3b7dff4a23e3a253f216a7b7c329e8cc
YuEgor <rezervdesu15@gmail.com>
2022-09-26 22:47:27 +0800

    Initial commit new start

--- VectorLab3Tests/VectorTests.cs
+++ VectorLab3Tests/VectorTests.cs
@@ -17,49 +17,4 @@ namespace VectorLab3.Tests
             var coordinateX = new Vector(3);
             Assert.AreEqual(3, coordinateX.Verbose());
         }
-
-        [TestMethod()]
-        public void VectorCoordinateSum()
-        {
-            var coordinateX = new Vector(3);
-            coordinateX = coordinateX + 3;
-            Assert.AreEqual(6, coordinateX.Verbose());
-        }
-
-        [TestMethod()]
-        public void VectorCoordinateDelete()
-        {
-            var coordinateX = new Vector(3);
-            coordinateX = coordinateX - 3;
-            Assert.AreEqual(0, coordinateX.Verbose());
-        }
-        [TestMethod()]
-        public void VectorCoordinateMultiply()
-        {
-            var coordinateX = new Vector(3);
-            coordinateX = coordinateX * 3;
-            Assert.AreEqual(9, coordinateX.Verbose());
-        }
-        [TestMethod()]
-        public void TwoVectorCoordinateSum()
-        {
-            var w = new Vector(3);
-            var e = new Vector(3);
-            Assert.AreEqual(6, (w+e).Verbose());
-        }
-        [TestMethod()]
-        public void VectorCoordinateExtent()
-        {
-            var coordinateX = new Vector(3);
-            coordinateX = Vector.Extent(coordinateX,3);
-            Assert.AreEqual(27, coordinateX.Verbose());
-        }
-        [TestMethod()]
-        public void VectorCoordinateRadical()
-        {
-            var coordinateX = new Vector(9);
-            coordinateX = Vector.Radical(coordinateX);
-            Assert.AreEqual(3, coordinateX.Verbose());
-        }
-    }
 }
